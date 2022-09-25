d060c2c64731abd85b4457dc412772c71ba03273
kanngji <50470748+kanngji@users.noreply.github.com>
2022-09-25 19:44:26 +0900

    ejs routes 연결까지

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const app = express();
 
 const PORT = 4000;
 
-app.use("/", (req, res) => {
-  res.send("hi");
-});
+// 라우터 연결
+const indexRouter = require("./routes/index");
+const chatRouter = require("./routes/chat");
+const loginRouter = require("./routes/login");
+const addressRouter = require("./routes/address");
+const registerRouter = require("./routes/register");
 
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use("/chat", chatRouter);
+app.use("/login", loginRouter);
+app.use("/address", addressRouter);
+app.use("/register", registerRouter);
 app.listen(`${PORT}`, () => {
   console.log(`이 서버는 ${PORT}포트에서 실행중입니다`);
 });
